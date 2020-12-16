80ad60081d546bf8ea51f0c7fc8269776da4bb6a
Ryoto Sasaki <ryoto@sasakinoMacBook-Air.local>
2020-12-16 22:45:45 +0900

    [add]

---
+++ build/client/scripts/inital_build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# terminalにて
+#     Dockerfile COPY & RUN & ENTRYPOINT commentout
+#     docker-compose up -d
+#     docker exec -i -t vue sh
+#     vue create vue_project
---
+++ build/client/scripts/nomal_build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cd vue_project
+# install node_modules
+npm install 
+
+# development server
+npm run serve
+# bilid project
